87ca8505eac039dc933b81b3b7903eda0ddd0d55
Abdul Khan <akhan@lcs.on.ca>
2022-03-25 12:30:31 -0400

    Changed the variables around and added textfields for each variable. Also added picker for category selection

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,7 +13,21 @@ struct ContentView: View {
     
     @State private var purchaseCost = ""
     @State private var description = ""
-    @State private var categories = ["Grocery", "Luxury", "Charity", "Other"]
+    var currentCategory: String {
+        switch selectedCategory {
+        case 0:
+            return "Grocery"
+        case 1:
+            return "Luxury"
+        case 2:
+            return "Charity"
+        default:
+            return "Other"
+        }
+    }
+    @State private var selectedCategory = 0
+    
+    let categories = ["Grocery", "Luxury", "Charity", "Other"]
     
     @State private var totalSpent: Double = 0
     
@@ -22,9 +36,15 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section(header: Text("Purchase Information")) {
-                    Text("")
+                    TextField("Description", text: $description)
+                    TextField("Cost", text: $purchaseCost)
+                    Picker("Select Category", selection: $selectedCategory) {
+                        ForEach(0 ..< categories.count, id: \.self) {
+                            Text(self.categories[$0])
+                        }
+                    } .pickerStyle(SegmentedPickerStyle())
                 }
-            }
+            }.navigationBarTitle("Budget", displayMode: .inline)
         }
         
     }
