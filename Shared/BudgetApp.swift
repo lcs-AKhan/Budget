a0834f23ed4d226505b1a03c078810354a3a1c13
Abdul Khan <akhan@lcs.on.ca>
2022-03-25 11:11:28 -0400

    Initial Commit

---
+++ Shared/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Shared
+//
+//  Created by Abdul-CS on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
