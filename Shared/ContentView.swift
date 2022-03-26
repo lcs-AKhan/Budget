7fe070bd8121b42a7cb329f3c04d70865e5cba95
Abdul Khan <akhan@lcs.on.ca>
2022-03-26 10:10:28 -0400

    Created function for adding a new purchase to the purchases array

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,40 +12,54 @@ struct ContentView: View {
     @State var purchases: [Purchase] = []
     
     @State private var purchaseCost = ""
+    
     @State private var description = ""
+    
     var currentCategory: String {
-        switch selectedCategory {
-        case 0:
-            return "Grocery"
-        case 1:
-            return "Luxury"
-        case 2:
-            return "Charity"
-        default:
-            return "Other"
-        }
+        
+        categories[selectedCategory]
+        
     }
+    
+    @State private var totalCost: Double = 0
+    
     @State private var selectedCategory = 0
     
     let categories = ["Grocery", "Luxury", "Charity", "Other"]
-    
-    @State private var totalSpent: Double = 0
-    
+        
     var body: some View {
         
         NavigationView {
+            
             Form {
+                
                 Section(header: Text("Purchase Information")) {
+                    
                     TextField("Description", text: $description)
+                    
                     TextField("Cost", text: $purchaseCost)
+                    
                     Picker("Select Category", selection: $selectedCategory) {
+                        
                         ForEach(0 ..< categories.count, id: \.self) {
+                            
                             Text(self.categories[$0])
+                            
                         }
                     } .pickerStyle(SegmentedPickerStyle())
                 }
             }.navigationBarTitle("Budget", displayMode: .inline)
         }
+    }
+    func addToPurchases() {
+        
+        totalCost += Double(purchaseCost) ?? 0
+        
+        let newPurchase = Purchase(description: description,
+                                  cost: Double(purchaseCost) ?? 0,
+                                  category: categories[selectedCategory])
+        
+        purchases.insert(newPurchase, at: 0)
         
     }
 }
