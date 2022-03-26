4de92402390a2124c3832e2df34873b29052bd6b
Abdul Khan <akhan@lcs.on.ca>
2022-03-26 10:39:19 -0400

    Added enum for purchase visibility in the list I will create of all user purchases

---
+++ Shared/PurchaseVisibility.swift
@@ -0,0 +1,19 @@
+//
+//  PurchaseVisibility.swift
+//  Budget
+//
+//  Created by Abdul-CS on 2022-03-26.
+//
+
+import Foundation
+
+enum PurchaseVisibility: String {
+    
+    case grocery = "Grocery"
+    case luxury = "Luxury"
+    case charity = "Charity"
+    case other = "Other"
+    
+}
+
+//let categories = ["Grocery", "Luxury", "Charity", "Other"]
