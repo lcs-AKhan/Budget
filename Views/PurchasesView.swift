ccd2ca7559a76299331a2aae370b876339b90619
Abdul Khan <akhan@lcs.on.ca>
2022-03-28 14:45:19 -0400

    Created filter function and picker for user to choose to sort list by category

--- Views/PurchasesView.swift
+++ Views/PurchasesView.swift
@@ -15,11 +15,50 @@ struct PurchasesView: View {
     var body: some View {
         
         Form {
+            Section {
+                Picker("Category", selection: $selectedPurchaseVisibility) {
+                    Text(PurchaseVisibility.all.rawValue)
+                        .tag(PurchaseVisibility.all)
+                    Text(PurchaseVisibility.grocery.rawValue)
+                        .tag(PurchaseVisibility.grocery)
+                    Text(PurchaseVisibility.luxury.rawValue)
+                        .tag(PurchaseVisibility.luxury)
+                    Text(PurchaseVisibility.charity.rawValue)
+                        .tag(PurchaseVisibility.charity)
+                    Text(PurchaseVisibility.other.rawValue)
+                        .tag(PurchaseVisibility.other)
+                }
+            }
+            Section {
                 List(purchases) { purchase in
                     Text("\(purchase.description) - $\(purchase.cost, specifier: "%.2f") - \(purchase.category)")
                 }
             }
-        
+        }
+    }
+    func filter(_ listOfPurchases: [Purchase], by visibility: PurchaseVisibility) -> [Purchase] {
+                        
+                if visibility == .all {
+                    return listOfPurchases
+                } else {
+                    
+                    var filteredPurchases: [Purchase] = []
+                            
+                    for currentPurchase in listOfPurchases {
+                                
+                    if visibility == .grocery && currentPurchase.category == "Grocery" {
+                        filteredPurchases.insert(currentPurchase, at: 0)
+                    } else if visibility == .luxury && currentPurchase.category == "Luxury" {
+                        filteredPurchases.insert(currentPurchase, at: 0)
+                    } else if visibility == .charity && currentPurchase.category == "Charity" {
+                        filteredPurchases.insert(currentPurchase, at: 0)
+                    } else if visibility == .other && currentPurchase.category == "Other" {
+                        filteredPurchases.insert(currentPurchase, at: 0)
+                    }
+                            
+                    }
+                    return filteredPurchases
+                }
     }
 }
 
